c42a66f6516b7d76dd16e31606fb47e47d9de143
itggot-elias-stenhede <elias.stenhedejohansson@itggot.se>
2019-05-09 11:00:56 +0200

    dfghjk

--- app.rb
+++ app.rb
@@ -2,8 +2,11 @@ require 'sinatra'
 require 'slim'
 enable :sessions
 require_relative './model.rb'
+include MyModule
 
 helpers do
+    #Returns true or false depending if user is logged in
+    #
     def permission()
         if session[:loggedin]
             return true
@@ -11,12 +14,16 @@ helpers do
             return false
         end
     end    
-
+    #Returns currently logged in user's id
+    #
     def id()
         return session[:userdata][1]
     end
 end
 
+
+# Displays homepage differently depending if user is logged in or not
+#
 get ("/") do
     if permission()
         slim(:profile)
@@ -25,19 +32,65 @@ get ("/") do
     end
 end
 
+# Displays the menu
+#
 get ("/menu") do
     pizzas()
     slim(:menu, locals: {pizzas: pizzas()})
 end
 
+# Displays info page
+#
 get ("/info") do
     slim(:info)
 end
 
+# Displays a register form
+#
 get ("/register") do
     slim(:register)
 end
 
+# Displays a login form
+#
+get ("/login") do
+    slim(:login)
+end
+
+# Displays a page telling user to log in
+#
+get ("/permission") do
+    slim(:permission)
+end
+
+# Displays a profile page
+#
+get ("/profile") do
+    if permission()
+        slim(:profile)
+    else
+        redirect ("/permission")
+    end
+end
+
+# Displays a the order page with the avalible pizzas
+#
+get ("/order") do
+    if permission()
+        session[:cart] = cart(id())
+        slim(:order, locals: {pizzas: pizzas()})
+    else
+        redirect ("/permission")
+    end
+end
+
+# Attempts to register a new user and if possible redirects to /login, else displays error message
+#
+# @param [String] Name, Enter name
+# @param [String] Passowrd, Enter password
+# @param [String] Phone, Enter phone number
+#
+# @see model#register
 post ("/register") do
     status = register(params)
     if status[:status] == :error
@@ -47,10 +100,12 @@ post ("/register") do
     end
 end
 
-get ("/login") do
-    slim(:login)
-end
-
+# Attempts login and updates the session
+#
+# @param [String] Phone, Telefonnummer
+# @param [String] Passowrd, Lösenord
+#
+# @see model#login
 post ("/login") do
     status = login(params)
     if status[:status] == :error
@@ -62,29 +117,11 @@ post ("/login") do
     end
 end
 
-get ("/permission") do
-    slim(:permission)
-end
-
-get ("/profile") do
-    if permission()
-        slim(:profile)
-    else
-        redirect ("/permission")
-    end
-end
-
-
-
-get ("/order") do
-    if permission()
-        session[:cart] = cart(id())
-        slim(:order, locals: {pizzas: pizzas()})
-    else
-        redirect ("/permission")
-    end
-end
-
+# Adds a pizza to cart
+#
+# @param [String] Phone, Telefonnummer
+#
+# @see Model#addtocart
 post ("/addtocart") do
     if permission()
         id = id()
@@ -95,6 +132,10 @@ post ("/addtocart") do
     end
 end
 
+# Creates an order
+# 
+# @param [String] sum, adds sum to invoice
+# @see model#order
 post ("/order") do
     if permission()
         id = id()
@@ -105,6 +146,10 @@ post ("/order") do
     end
 end
 
+# Shows page with logged in user's invoice
+# 
+# @param [String] 
+# @see model#invoice
 get ("/invoices") do
     if permission()
         slim(:invoices, locals: {data: invoice(id())})
@@ -113,6 +158,8 @@ get ("/invoices") do
     end
 end
 
+# Destroys session and redirects to homepage
+# 
 get ("/logout") do
     session.destroy
     redirect("/")
--- model.rb
+++ model.rb
@@ -1,128 +1,189 @@
 require 'sqlite3'
 require 'bcrypt'
 
-def database()
-    return SQLite3::Database.new("db/database.db")
-    db.results_as_hash = true
-end
-
-def register(params)
-    db = database()
-    if params["Name"].length < 2
-        return {status: :error, message: "Name has to be longer than 3 characters!"}
-    elsif params["Password"].length < 5
-        return {status: :error, message: "Password needs to contain 6 or more characters!"}
-    end
-    
-    if db.execute("SELECT Phone FROM users WHERE Phone = #{params["Phone"]}") == []
-        hashed_pass = BCrypt::Password.create("#{params["Password"]}")
-        db.execute("INSERT INTO users (Name, Password, Phone) VALUES (?, ?, ?)", params["Name"], hashed_pass, params["Phone"])
-        return {status: :ok}
-    else
-        return {status: :error, message: "Phone number is already in use"}
-    end
-end
-
-def login(params)
-    db = database()
-    if params["Phone"].length > 0
-        hashed_pass = db.execute("SELECT Password FROM users WHERE Phone = ?", params["Phone"])
-    else
-        return {status: :error, message: "Invalid Credentials"}
+module MyModule
+    #Returns the database
+    #
+    def database()
+        return SQLite3::Database.new("db/database.db")
     end
 
-    if hashed_pass.length == 0
-        return {status: :error, message: "Invalid Credentials"}
-    else
-        hashed_pass = hashed_pass.first.first
-    end
-
-    if BCrypt::Password.new(hashed_pass) == params["Password"]
-        userdata = db.execute("SELECT Name, Id FROM users WHERE Phone = ?", params["Phone"])
-        return {status: :ok, userdata: userdata.first}
-    else
-        return {status: :error, message: "Invalid Credentials"}
-    end
-end
-
-def pizzas()
-    db = database()
-    return db.execute("SELECT PizzaName, Price, Id FROM pizzas")
-end
-
-def cart(id)
-    db = database()
-    return db.execute("SELECT PizzaName, Price, PizzaId FROM pizzas INNER JOIN carts ON UserId = ? WHERE PizzaId = Id", id)
-end
-
-def addtocart(params, id)
-    db = database()
-    db.execute("INSERT INTO carts (UserId, PizzaId) VALUES (?, ?)", id, params["pizza-id"])
-end
-
-def order(params, id)
-    db = database()
-    cart = cart(id)
-    i = 0
-    time = Time.now.to_s[0..18]
-    db.execute("INSERT INTO invoices (Sum, UserId, Timestamp) VALUES (?, ?, ?)", params["sum"].to_i, id, time)
-    invoiceid = db.execute("SELECT Id FROM invoices WHERE Timestamp = ?", time)
-    cart.each do |item|
-        print item
-        db.execute("INSERT INTO pizza_invoice_relation (InvoiceId, PizzaId) VALUES (?, ?)", invoiceid, item[2])
-    end
-    delete_cart(id)
-end
-
-def delete_cart(id)
-    db = database()
-    db.execute("DELETE FROM carts WHERE UserId = ?", id)
-end
-
-def occurs(indata)
-    i = 0
-    pizzas = Hash.new 0
-    prices = Hash.new 0
-    pizza_output = []
-    prices_output = []
-    sums = []
-    timestamps = []
-    name = []
-    while i < indata.length
-        if indata[i+1] != nil && indata[i][5] == indata[i+1][5] 
-            pizzas[indata[i][0]] += 1
-            prices[indata[i][2]] += 1
-        else
-            pizzas[indata[i][0]] += 1
-            pizza_output << pizzas
-            pizzas = Hash.new 0
-            prices[indata[i][2]] += 1
-            prices_output << prices
-            prices = Hash.new 0
-            name << indata.first[1]
-            sums << indata[i][3]
-            timestamps << indata[i][4]
+    # Attempts to register user
+    #
+    # @param [String] params phone
+    # @param [String] params password
+    # @param [String] params name
+    #
+    # @return [Hash]
+    #   * :status [Symbol] whether an error occured (:ok or :error)
+    #   * :message [String] the error message
+    def register(params)
+        db = database()
+        if params["Name"].length < 2
+            return {status: :error, message: "Name has to be longer than 3 characters!"}
+        elsif params["Password"].length < 5
+            return {status: :error, message: "Password needs to contain 6 or more characters!"}
         end
-        i += 1
+        
+        if db.execute("SELECT Phone FROM users WHERE Phone = #{params["Phone"]}") == []
+            hashed_pass = BCrypt::Password.create("#{params["Password"]}")
+            db.execute("INSERT INTO users (Name, Password, Phone) VALUES (?, ?, ?)", params["Name"], hashed_pass, params["Phone"])
+            return {status: :ok}
+        else
+            return {status: :error, message: "Phone number is already in use"}
+        end
     end
-    if indata[i-1] != nil
-        sums << indata[i-1][3]
-        timestamps << indata[i-1][4]
-    end
-    return pizza_output, prices_output, sums, timestamps, name
-end
 
-def invoice(id)
-    db = database()
-    data = (db.execute("SELECT PizzaName, Name, Price, Sum, Timestamp, InvoiceId
-    FROM invoices 
-    INNER JOIN pizza_invoice_relation 
-    INNER JOIN pizzas 
-    INNER JOIN users ON invoices.UserId = ?
-    WHERE pizza_invoice_relation.PizzaId = pizzas.Id 
-    AND users.Id = invoices.UserId 
-	AND pizza_invoice_relation.InvoiceId = invoices.Id
-    ORDER BY Timestamp
-    ", id))
-    return occurs(data)
+    # Attempts to login
+    #
+    # @param [String] params phone
+    # @param [String] params password
+    #
+    # @return [Hash]
+    #   * :status [Symbol] whether an error occured (:ok or :error)
+    #   * :message [String] the error message
+    def login(params)
+        db = database()
+        if params["Phone"].length > 0
+            hashed_pass = db.execute("SELECT Password FROM users WHERE Phone = ?", params["Phone"])
+        else
+            return {status: :error, message: "Invalid Credentials"}
+        end
+
+        if hashed_pass.length == 0
+            return {status: :error, message: "Invalid Credentials"}
+        else
+            hashed_pass = hashed_pass.first.first
+        end
+
+        if BCrypt::Password.new(hashed_pass) == params["Password"]
+            userdata = db.execute("SELECT Name, Id FROM users WHERE Phone = ?", params["Phone"])
+            return {status: :ok, userdata: userdata.first}
+        else
+            return {status: :error, message: "Invalid Credentials"}
+        end
+    end
+
+    # Fetches pizzas
+    #
+    # @return [Array]
+    #   PizzaName, Price, PizzaId
+    def pizzas()
+        db = database()
+        return db.execute("SELECT PizzaName, Price, Id FROM pizzas")
+    end
+
+    # Fetches content in cart
+    #
+    # @param [Integer] User Id
+    #
+    # @return [Array]
+    #   PizzaName, Price, PizzaId
+    def cart(id)
+        db = database()
+        return db.execute("SELECT PizzaName, Price, PizzaId FROM pizzas INNER JOIN carts ON UserId = ? WHERE PizzaId = Id", id)
+    end
+
+    # Adds pizza to cart in database
+    #
+    # @param [Integer] User Id
+    # @param [Integer] Pizza Id
+    def addtocart(params, id)
+        db = database()
+        db.execute("INSERT INTO carts (UserId, PizzaId) VALUES (?, ?)", id, params["pizza-id"])
+    end
+
+    # Adds all pizzas in cart to an invoice calls the function delete_cart
+    #
+    # @param [String] Sum
+    def order(params, id)
+        db = database()
+        cart = cart(id)
+        i = 0
+        time = Time.now.to_s[0..18]
+        db.execute("INSERT INTO invoices (Sum, UserId, Timestamp) VALUES (?, ?, ?)", params["sum"].to_i, id, time)
+        invoiceid = db.execute("SELECT Id FROM invoices WHERE Timestamp = ?", time)
+        cart.each do |item|
+            print item
+            db.execute("INSERT INTO pizza_invoice_relation (InvoiceId, PizzaId) VALUES (?, ?)", invoiceid, item[2])
+        end
+        delete_cart(id)
+    end
+
+    # Delete all id's from specific user's cart
+    #
+    # @param [String] UserId
+    def delete_cart(id)
+        db = database()
+        db.execute("DELETE FROM carts WHERE UserId = ?", id)
+    end
+
+    # Rearranges raw data from database to a more structured format
+    #
+    # @param [String] PizzaName
+    # @param [String] Name
+    # @param [String] Price
+    # @param [String] Sum
+    # @param [String] Timestamp
+    # @param [String] InvoiceId
+        # @return [Array]
+            # @return [Hash]
+                # pizza_output
+            # @return [Hash]
+                # prices_output
+            # @return [Array]
+                # sums
+            # @return [Array]
+                # timestamps
+            # @return [Array]
+                # name
+    def occurs(indata)
+        i = 0
+        pizzas = Hash.new 0
+        prices = Hash.new 0
+        pizza_output = []
+        prices_output = []
+        sums = []
+        timestamps = []
+        name = []
+        while i < indata.length
+            if indata[i+1] != nil && indata[i][5] == indata[i+1][5] 
+                pizzas[indata[i][0]] += 1
+                prices[indata[i][2]] += 1
+            else
+                pizzas[indata[i][0]] += 1
+                pizza_output << pizzas
+                pizzas = Hash.new 0
+                prices[indata[i][2]] += 1
+                prices_output << prices
+                prices = Hash.new 0
+                name << indata.first[1]
+                sums << indata[i][3]
+                timestamps << indata[i][4]
+            end
+            i += 1
+        end
+        if indata[i-1] != nil
+            sums << indata[i-1][3]
+            timestamps << indata[i-1][4]
+        end
+        return pizza_output, prices_output, sums, timestamps, name
+    end
+
+    # Fetches data from database
+    #
+    def invoice(id)
+        db = database()
+        data = (db.execute("SELECT PizzaName, Name, Price, Sum, Timestamp, InvoiceId
+        FROM invoices 
+        INNER JOIN pizza_invoice_relation 
+        INNER JOIN pizzas 
+        INNER JOIN users ON invoices.UserId = ?
+        WHERE pizza_invoice_relation.PizzaId = pizzas.Id 
+        AND users.Id = invoices.UserId 
+        AND pizza_invoice_relation.InvoiceId = invoices.Id
+        ORDER BY Timestamp
+        ", id))
+        return occurs(data)
+    end
 end
